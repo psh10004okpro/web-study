030b472ed1379fa27bc0be8c543c306668cf0117
psh10004okpro <81824536+psh10004okpro@users.noreply.github.com>
2024-05-17 17:19:10 +0900

    Add files via upload

--- appLink.js
+++ appLink.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
     appLink.addEventListener('click', function(e) {
         e.preventDefault();
 
-        const appScheme = `unitydl://mindvr?room=${encodeURIComponent(page)}}`;
+        const appScheme = `unitydl://mindvr?room=${encodeURIComponent(page)}`;
         
         if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
             // iOS
