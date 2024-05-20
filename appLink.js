322032e477319cebafd11755661eeadc8f8d6759
psh10004okpro <81824536+psh10004okpro@users.noreply.github.com>
2024-05-20 15:54:19 +0900

    Update appLink.js

--- appLink.js
+++ appLink.js
@@ -10,8 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function startApp()
     {
-        const urlParams = new URLSearchParams(window.location.search);
-        const page = urlParams.get("room");
+
+        const url = new URL(window.location.search);
+        
+        //const urlParams = new URLSearchParams(window.location.search);
+        const page = url.get("room");
         //const appScheme = 'unitydl://mindvr?' + page;
         const appScheme = 'unitydl://mindvr?2';
         if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
