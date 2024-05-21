1a68b828489649f4040e8d41c4eb9da93251afb5
psh10004okpro <81824536+psh10004okpro@users.noreply.github.com>
2024-05-21 12:51:06 +0900

    Update appLink.js

--- appLink.js
+++ appLink.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const appStoreURLiOS = 'https://apps.apple.com/app/id6449755259'; // 앱 스토어 URL (App Store 예시)
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
     
-    document.getElementById('appLink').click();
+    //document.getElementById('appLink').click();
     // function startApp()
     // {
     //     const appScheme = 'unitydl://mindvr?2';
