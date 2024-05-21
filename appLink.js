0408aefc3c84f8cc328a4ebb70451fb3ced5dce2
psh10004okpro <81824536+psh10004okpro@users.noreply.github.com>
2024-05-21 14:19:32 +0900

    Update appLink.js

--- appLink.js
+++ appLink.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const appLinkButton = document.getElementById('appLink');
+    //const appLinkButton = document.getElementById('appLink');
     const urlParams = new URLSearchParams(window.location.search);
     const page = urlParams.get('room');
 
@@ -8,6 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const appStoreURL = 'https://play.google.com/store/apps/details?id=com.Mindvridge'; // 앱 스토어 URL (Google Play 예시)
     const appStoreURLiOS = 'https://apps.apple.com/app/id6449755259'; // 앱 스토어 URL (App Store 예시)
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    const appScheme = `unitydl://mindvr?${encodeURIComponent(page)}`;
+    //const appScheme = `unitydl://mindvr?2`;
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        // iOS
+        openApp(appScheme, appStoreURLiOS);
+    } else if (/android/i.test(userAgent)) {
+        // Android
+        openApp(appScheme, appStoreURL);
+    } else {
+        // Other platforms or fallback
+        window.location.href = appStoreURL;
+    }
     
     //document.getElementById('appLink').click();
     // function startApp()
@@ -29,25 +43,25 @@ document.addEventListener("DOMContentLoaded", function() {
     //
     //
     // }
-    
-    
-    appLinkButton.addEventListener('click', function(e) {
-        e.preventDefault();
 
-        const appScheme = `unitydl://mindvr?${encodeURIComponent(page)}`;
-         //const appScheme = `unitydl://mindvr?2`;
 
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            // iOS
-            openApp(appScheme, appStoreURLiOS);
-        } else if (/android/i.test(userAgent)) {
-            // Android
-            openApp(appScheme, appStoreURL);
-        } else {
-            // Other platforms or fallback
-            window.location.href = appStoreURL;
-        }
-    });
+    // appLinkButton.addEventListener('click', function(e) {
+    //     e.preventDefault();
+    //
+    //     const appScheme = `unitydl://mindvr?${encodeURIComponent(page)}`;
+    //     //const appScheme = `unitydl://mindvr?2`;
+    //
+    //     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    //         // iOS
+    //         openApp(appScheme, appStoreURLiOS);
+    //     } else if (/android/i.test(userAgent)) {
+    //         // Android
+    //         openApp(appScheme, appStoreURL);
+    //     } else {
+    //         // Other platforms or fallback
+    //         window.location.href = appStoreURL;
+    //     }
+    // });
 
     function openApp(appScheme, appStoreURL) {
         const startTime = new Date().getTime();
@@ -67,9 +81,9 @@ document.addEventListener("DOMContentLoaded", function() {
             if (endTime - startTime < timeout + 100) {
                 window.location.href = appStoreURL;
             }
-            
+
             window.close();
-            
+
         }, timeout);
     }
 
