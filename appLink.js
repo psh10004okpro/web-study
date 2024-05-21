73d156148976feb7431087409bc1bb192b4ab22e
psh10004okpro <81824536+psh10004okpro@users.noreply.github.com>
2024-05-21 16:10:15 +0900

    Update appLink.js

--- appLink.js
+++ appLink.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     //const appLinkButton = document.getElementById('appLink');
-    //const urlParams = new URLSearchParams(window.location.search);
-    //const page = urlParams.get('room');
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = urlParams.get('room');
 
 
     //const appScheme = 'unitydl://mindvr?2'; // 앱 스킴 (앱에서 정의한 스킴을 사용)
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const appStoreURLiOS = 'https://apps.apple.com/app/id6449755259'; // 앱 스토어 URL (App Store 예시)
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    //const appScheme = `unitydl://mindvr?${encodeURIComponent(page)}`;
-    const appScheme = `unitydl://mindvr?2`;
+    const appScheme = `unitydl://mindvr?${encodeURIComponent(page)}`;
+    //const appScheme = `unitydl://mindvr?2`;
 
     openApp(appScheme, appStoreURL);
     
@@ -68,27 +68,28 @@ document.addEventListener("DOMContentLoaded", function() {
     function openApp(appScheme, appStoreURL) {
         const startTime = new Date().getTime();
         const timeout = 1500;
-        const iframe = document.createElement('iframe');
-        iframe.style.border = 'none';
-        iframe.style.width = '1px';
-        iframe.style.height = '1px';
-        iframe.onload = function() {
-            document.body.removeChild(iframe);
-        };
-        iframe.src = appScheme;
-        document.body.appendChild(iframe);
-        //console.log("appScheme : " + appScheme)
-        
-   // setTimeout(function() {
-        //     const endTime = new Date().getTime();
-        //     if (endTime - startTime < timeout + 100) {
-        //         window.location.href = appStoreURL;
-        //     }
-        //
-        //     window.close();
-        //
-        // }, timeout);
-    };
+        window.location.href = appScheme;
+        // const iframe = document.createElement('iframe');
+        // iframe.style.border = 'none';
+        // iframe.style.width = '1px';
+        // iframe.style.height = '1px';
+        // iframe.onload = function() {
+        //     document.body.removeChild(iframe);
+        // };
+        // iframe.src = appScheme;
+        // document.body.appendChild(iframe);
+        // console.log("appScheme : " + appScheme)
+        setTimeout(function() {
+            const endTime = new Date().getTime();
+            if (endTime - startTime < timeout + 100) {
+                window.location.href = appStoreURL;
+            }
+            else {
+                window.close();
+            }
+
+        }, timeout);
+    }
 
     //startApp();
 });
