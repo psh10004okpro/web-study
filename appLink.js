bd055dfa310bd8d30d23a4eeaaaa3f66e91d21d8
psh10004okpro <81824536+psh10004okpro@users.noreply.github.com>
2024-05-30 10:10:17 +0900

    Update appLink.js

--- appLink.js
+++ appLink.js
@@ -10,18 +10,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const appScheme = `unitydl://mindvridge?${encodeURIComponent(page)}&${encodeURIComponent(page2)}`;
 
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-     // iOS
-         openApp(appScheme, appStoreURLiOS);
-     } else if (/android/i.test(userAgent)) {
-         // Android
-         openApp(appScheme, appStoreURL);
-     } else {
+        // iOS
+        openApp(appScheme, appStoreURLiOS);
+    } else if (/android/i.test(userAgent)) {
+        // Android
+        openApp(appScheme, appStoreURL);
+    } else {
         // Other platforms or fallback
         window.location.href = appStoreURL;
     }
-    
-    //openApp(appScheme, appStoreURL);
-    
+
     function openApp(appScheme, appStoreURL) {
         const startTime = new Date().getTime();
         const timeout = 1500;
@@ -31,10 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
             if (endTime - startTime < timeout + 100) {
                 window.location.href = appStoreURL;
             }
-            else {
-                window.close();
-            }
-
+            // `window.close();` 제거됨: 대부분의 브라우저에서는 동작하지 않음.
         }, timeout);
     }
 });
